628b9943af3415916874380fb29ab8568a9fc8bd
정형주 <ggumihj@naver.com>
2025-08-04 13:05:09 +0900

    필요없는 코드 삭제

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: HomeView, name: 'Home' },
