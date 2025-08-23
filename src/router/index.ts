fe56712ecc9695cc0dd0b849dc84278e9d51e196
HJ_Jung <ggumihj@naver.com>
2025-08-23 14:49:38 +0900

    fix: 필요없는 파일 삭제

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import TestView from '@/components/test.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/home' },
   { path: '/home', name: 'Home', component: HomeView },
-  { path: '/test', name: 'Test', component: TestView },
 ]
 
 const router = createRouter({
